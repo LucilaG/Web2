2ceaa49dacff19ff57a37e48dffb34f865e09ff4
LucilaG <gaudiolucila@gmail.com>
2018-11-21 13:40:07 -0300

    main update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 'use strict'
 let templateComentario;
 
+document.querySelector('#enviarComentario').addEventListener('click', e => {
+    e.preventDefault();
+    enviarComentario()
+});
 
 fetch('js/templates/comentariosCine.handlebars')
     .then(response => response.text())
@@ -39,3 +43,26 @@ function mostrarComentario(jsonComentario) {
     let html = templateComentario(context);
     document.querySelector("#cine-container").innerHTML = html;
 }
+
+function enviarComentario() {
+    let id = document.querySelector("#id").value;
+    let comentario = document.querySelector('#comentario').value;
+    let puntaje = document.querySelector('#puntaje').value;
+    let id_cine = document.querySelector("#id_cine").value;
+    let objetoJSON = {
+        "id": id,
+        "comentario": comentario,
+        "puntaje": puntaje,
+        "id_cine": id_cine
+    }
+    console.log(objetoJSON);
+    fetch("api/comentario/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(objetoJSON)
+    }).then(response =>
+        getComentario()
+    );
+  }
